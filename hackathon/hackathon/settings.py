787349a78a1f5466ff4746b68ff36e7e7fac5e4a
sweetysingh <sweetysingh@1609>
2019-09-27 20:23:12 +0530

    v0.3 Added User,Psychologist

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -15,7 +15,6 @@ from .local_settings import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -41,6 +40,8 @@ INSTALLED_APPS = [
     'chat_bot',
     'social_django',
 ]
+
+AUTH_USER_MODEL = 'main.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- hackathon/main/models.py
+++ hackathon/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class User(AbstractUser):
+   is_user = models.BooleanField(default=True)
+   is_psychologist = models.BooleanField(default=False)
+
+class Client(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+
+class Psychologist(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=15)
+    highest_qualifications = models.CharField(max_length=150)
+    Clinic_location = models.TextField()
